dbbca68d44a27c1227654240240b4775f9aaf0ae
Mike Stanley <mcstanle@andrew.cmu.edu>
2020-06-02 21:11:38 -0400

    added script to create smoothed flux txt files

--- utils/generate_normalized_flux_data.py
+++ utils/generate_normalized_flux_data.py
@@ -7,19 +7,137 @@ Created       : June 2, 2020
 Last Modified : June 2, 2020
 """
 
+from glob import glob
+import numpy as np
+import xbpch
 
-def run():
+import carbonfluxtools.io as cio
+import carbonfluxtools.computation as ccomp
+
+
+def run(
+    true_flux_dir,
+    prior_flux_dir,
+    true_flux_prefix,
+    prior_flux_prefix,
+    tracerinfo_path,
+    diaginfo_path,
+    output_dir,
+    varname_oi='CO2_SRCE_CO2bf',
+    TOL=0.000001
+):
     """
     Run the steps required to make the scaled fluxes and write them to disk.
+
+    At the moment, this function scales the prior down to have the same global
+    flux as the truth.
+
+    Parameters:
+        true_flux_dir     (str)   : directory location of true fluxes
+        prior_flux_dir    (str)   : directory location of prior fluxes
+        true_flux_prefix  (str)   : e.g. 'nep.geos.4x5.2010.'
+        prior_flux_prefix (str)   : e.g. 'nep.geos.4x5.'
+        tracerinfo_path   (str)   : location of tracerinfo file for reading bpch
+        diaginfo_path     (str)   : location of diaginfo file for reading bpch
+        output_dir        (str)   : directory of txt output files
+        varname_oi        (str)   : variable to extract from the bpch objects
+        TOL               (float) : tolerance of new integrated flux
+
+    Returns:
+        writes daily flux txt files to output_dir
     """
-    print('hello world')
+    # find the sorted file names
+    prior_files = sorted(
+        glob(prior_flux_dir + '/' + prior_flux_prefix + '*'),
+        key=lambda x: int(x[-3:])
+    )
+    true_files = sorted(
+        glob(true_flux_dir + '/' + true_flux_prefix + '*'),
+        key=lambda x: int(x[-3:])
+    )
+
+    # read in the fluxes
+    prior_data = xbpch.open_mfbpchdataset(
+        prior_files,
+        dask=True,
+        tracerinfo_file=tracerinfo_path,
+        diaginfo_file=diaginfo_path
+    )
+    print('Prior fluxes acquired')
+    true_data = xbpch.open_mfbpchdataset(
+        true_files,
+        dask=True,
+        tracerinfo_file=tracerinfo_path,
+        diaginfo_file=diaginfo_path
+    )
+    print('True fluxes acquired')
+
+    # get longitude/latitude arrays
+    lons = prior_data.variables['lon'].values
+    lats = prior_data.variables['lat'].values
+
+    # extract flux arrays from the xbpch objects
+    prior_arr = prior_data.variables[varname_oi].values
+    true_arr = true_data.variables[varname_oi].values
+
+    # determine global integral of prior and posterior
+    prior_global_flux = ccomp.compute_global_flux(
+        flux_arr=prior_arr, lons=lons, lats=lats
+    )
+    true_global_flux = ccomp.compute_global_flux(
+        flux_arr=true_arr, lons=lons, lats=lats
+    )
+
+    # find the scalar multiplier
+    scl_mult = true_global_flux / prior_global_flux
+    print('Scalar multiplier : %.5f' % scl_mult)
+
+    # scale the prior
+    prior_arr_scl = scl_mult * prior_arr
+
+    # compute new integrated flux
+    prior_global_flux_scl = ccomp.compute_global_flux(
+        flux_arr=prior_arr_scl, lons=lons, lats=lats
+    )
+    scl_mult_updated = true_global_flux / prior_global_flux_scl
+
+    if np.abs(scl_mult_updated - 1) > TOL:
+        print('Normalized flux exceeds tolerance: TOL = %.10f' % np.abs(scl_mult_updated - 1))
+        exit()
+
+    # write the new flux array to directory
+    cio.generate_txt_files_np(
+        flux_arr=prior_arr_scl,
+        bpch_files=prior_files,
+        output_dir=output_dir
+    )
 
 
 if __name__ == "__main__":
 
     # define some I/O constants
-    TRUE_FLUX_DIR = ''
-    PRIOR_FLUX_DIR = ''
+    BASE_DIR = '/Users/mikestanley/Research/Carbon_Flux'
+    PRIOR_FLUX_DIR = BASE_DIR + '/data/NEE_fluxes'
+    TRUE_FLUX_DIR = BASE_DIR + '/data/JULES'
+
+    # define prefixes
+    PRIOR_FLUX_PREFIX = 'nep.geos.4x5.'
+    TRUE_FLUX_PREFIX = 'nep.geos.4x5.2010.'
+
+    # tracer and diag paths
+    TRACERINFO_PATH = PRIOR_FLUX_DIR + '/tracerinfo.dat'
+    DIAGINFO_PATH = PRIOR_FLUX_DIR + '/diaginfo.dat'
+
+    # output directory
+    OUTPUT_DIR = BASE_DIR + '/data/NEE_fluxes_txt_scl'
 
     # create new scaled fluxes
-    run()
+    run(
+        true_flux_dir=TRUE_FLUX_DIR,
+        prior_flux_dir=PRIOR_FLUX_DIR,
+        true_flux_prefix=TRUE_FLUX_PREFIX,
+        prior_flux_prefix=PRIOR_FLUX_PREFIX,
+        tracerinfo_path=TRACERINFO_PATH,
+        diaginfo_path=DIAGINFO_PATH,
+        output_dir=OUTPUT_DIR
+    )
---
+++ utils/generate_smoothed_fluxes.py
@@ -0,0 +1,125 @@
+"""
+This script is primarily responsible for removing the diurnal cycle from
+fluxes of interest.
+
+INPUT :
+    - change FLUX_DIR
+
+OUTPUT :
+    - change OUTPUT_DIR
+
+Output files are txt files. When unpacking, indices move from fastest to
+slowest in the following order
+1. latitude
+2. longitude
+3. 3hr steps
+
+Author        : Mike Stanley
+Created       : June 2, 2020
+Last Modified : June 2, 2020
+"""
+
+from glob import glob
+import numpy as np
+import pandas as pd
+import xbpch
+
+import carbonfluxtools.io as cio
+
+
+def run(
+    flux_dir,
+    flux_prefix,
+    tracerinfo_path,
+    diaginfo_path,
+    output_dir,
+    window_size=8,
+    varname_oi='CO2_SRCE_CO2bf'
+):
+    """
+    Perform the steps requires to create the new flux files.
+
+    Parameters:
+        flux_dir        (str) : directory where daily flux files are found
+        flux_prefix     (str) : prefix of flux files
+        tracerinfo_path (str) : path to tracerinfo file for bpch read
+        diaginfo_path   (str) : path to diaginfo file for bpch read
+        output_dir      (str) : output directory location for new fluxes
+        window_size     (int) : window size for smoothing
+        varname_oi      (str) : variable of interest in the bpch files
+
+    Output:
+        creates smoothed daily flux files
+    """
+    # obtain the sorted flux file names
+    flux_files = sorted(
+        glob(flux_dir + '/' + flux_prefix + '*'),
+        key=lambda x: int(x[-3:])
+    )
+
+    # read in the fluxes
+    flux_data = xbpch.open_mfbpchdataset(
+        flux_files,
+        dask=True,
+        tracerinfo_file=tracerinfo_path,
+        diaginfo_file=diaginfo_path
+    )
+    print('Fluxes acquired')
+
+    # extract flux arrays from the xbpch objects
+    flux_arr = flux_data.variables[varname_oi].values
+
+    # get the dimensions of the above array for reshaping
+    arr_dims = flux_arr.shape
+
+    # reshape the above array to prepare for dataframe
+    flux_2d = flux_arr.flatten().reshape(arr_dims[0], arr_dims[1] * arr_dims[2])
+
+    # make the above into a dataframe
+    flux_df = pd.Dataframe(flux_2d)
+
+    # find the rolling average over time
+    flux_df_ra = flux_df.rolling(
+        window=window_size, axis=0
+    ).mean().interpolate(limit_direction='both')
+
+    # reshape back to original size
+    flux_arr_new = flux_df_ra.values.flatten().reshape(
+        arr_dims[0], arr_dims[1], arr_dims[2]
+    )
+
+    assert flux_arr_new.shape == arr_dims
+
+    # write the new flux array to directory
+    cio.generate_txt_files_np(
+        flux_arr=flux_arr_new,
+        bpch_files=flux_files,
+        output_dir=output_dir
+    )
+    print('Done.')
+
+
+if __name__ == "__main__":
+
+    # define I/O constants
+    BASE_DIR = '/Users/mikestanley/Research/Carbon_Flux'
+    FLUX_DIR = BASE_DIR + '/data/NEE_fluxes'
+
+    # define prefixes
+    FLUX_PREFIX = 'nep.geos.4x5.'
+
+    # tracer and diag paths
+    TRACERINFO_PATH = FLUX_DIR + '/tracerinfo.dat'
+    DIAGINFO_PATH = FLUX_DIR + '/diaginfo.dat'
+
+    # output directory
+    OUTPUT_DIR = BASE_DIR + '/data/NEE_fluxes_txt_scl'
+
+    # create the new smoothed fluxes
+    run(
+        flux_dir=FLUX_DIR,
+        flux_prefix=FLUX_PREFIX,
+        tracerinfo_path=TRACERINFO_PATH,
+        diaginfo_path=DIAGINFO_PATH,
+        output_dir=OUTPUT_DIR,
+    )
