91496ada2068ce7d4b6eb538607f5262c3678d0b
Mike Stanley <mcstanle@andrew.cmu.edu>
2020-09-30 10:07:41 -0400

    adding loop to script to iterate though monte carlo runs

--- monte_carlo/move_all_data.sh
+++ monte_carlo/move_all_data.sh
@@ -8,6 +8,7 @@ BASE_DIR=/glade/work/mcstanley/monte_carlo
 monte_carlo_run_num=1  # which overall run is being saved?
 ORIG_DIR=$BASE_DIR/V1
 NEW_DIR=$BASE_DIR/data_archive/run_$monte_carlo_run_num
+OPT_DATA_SUB_DIR=monte_carlo_element/runs/v8-02-01/geos5/OptData
 
 mkdir $NEW_DIR
 
@@ -15,4 +16,6 @@ mkdir $NEW_DIR
 cp -r $ORIG_DIR/results $NEW_DIR
 
 # copy each OptData
-#
+for file in $(ls $ORIG_DIR/src/element_*); do
+    echo $file/$OPT_DATA_SUB_DIR
+done
