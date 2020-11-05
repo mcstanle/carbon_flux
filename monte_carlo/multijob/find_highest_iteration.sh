cc0e2f2a7de98756f8ca9276b86451d82fc7c097
Mike Stanley <mcstanle@andrew.cmu.edu>
2020-11-04 21:40:56 -0500

    added script to find the last iteration of every ensemble element

---
+++ monte_carlo/multijob/find_highest_iteration.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Given a directory of optimization results, find the last optimized one.
+#
+# Author        : Mike Stanley
+# Created       : November 4, 2020
+# Last Modified : November 4, 2020
+# =============================================================================
+BASE_DIR=/glade/work/mcstanley/monte_carlo
+MULTIJOB_DIR=$BASE_DIR/multijob
+ENS_STEM=runs/v8-02-01/geos5/OptData
+
+# make a directory for the raw results
+mkdir -p $DEST_DIR/OptData
+
+# create a file to store the counts
+touch ./count_file.txt
+
+count=0
+for path in $MULTIJOB_DIR/ens*
+do
+
+    max_val=0
+    for sf_file in $path/$ENS_STEM/gctm.sf.*
+    do
+        opt_num=$(rev $sf_file | cut -c1-2)
+
+        if [ $opt_num -gt $max_val ]; then
+            max_val=$opt_num
+        fi
+    done
+    "Ens_"$count" = "$max_val >> ./count_file.txt
+done
