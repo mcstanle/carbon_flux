5cb06482fbe7518cea713869bff4af5c972960dc
Mike Stanley <mcstanle@andrew.cmu.edu>
2020-11-05 20:50:20 -0500

    modified script to count scale factor optimizations

--- monte_carlo/multijob/find_highest_iteration.sh
+++ monte_carlo/multijob/find_highest_iteration.sh
@@ -4,25 +4,27 @@
 #
 # Author        : Mike Stanley
 # Created       : November 4, 2020
-# Last Modified : November 4, 2020
+# Last Modified : November 5, 2020
 # =============================================================================
 BASE_DIR=/glade/work/mcstanley/monte_carlo
-MULTIJOB_DIR=$BASE_DIR/multijob
+MULTIJOB_DIR=$BASE_DIR/data_archive/run_3/OptData
 ENS_STEM=runs/v8-02-01/geos5/OptData
 
 # make a directory for the raw results
 # mkdir -p $DEST_DIR/OptData
 
 # create a file to store the counts
-FILENAME=count_file_2point25.txt
+FILENAME=count_file_3.txt
 touch ./$FILENAME
 
 count=0
-for path in $MULTIJOB_DIR/ens*
+# for path in $MULTIJOB_DIR/ens*
+for path in $MULTIJOB_DIR/element_*
 do
 
     max_val=0
-    for sf_file in $path/$ENS_STEM/gctm.sf.*
+    # for sf_file in $path/$ENS_STEM/gctm.sf.*
+    for sf_file in $path/gctm.sf.*
     do
         opt_num=$(echo $sf_file | rev | cut -c1-2 | rev)
 
