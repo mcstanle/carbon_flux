b573555eac38ce84b89e2937bb7924b2b2c9a25b
Mike Stanley <mcstanle@andrew.cmu.edu>
2020-11-06 09:45:31 -0500

    updated script to count finishing iteration for each ensemble element

--- monte_carlo/multijob/find_highest_iteration.sh
+++ monte_carlo/multijob/find_highest_iteration.sh
@@ -4,27 +4,28 @@
 #
 # Author        : Mike Stanley
 # Created       : November 4, 2020
-# Last Modified : November 5, 2020
+# Last Modified : November 6, 2020
 # =============================================================================
 BASE_DIR=/glade/work/mcstanley/monte_carlo
-MULTIJOB_DIR=$BASE_DIR/data_archive/run_3/OptData
+# MULTIJOB_DIR=$BASE_DIR/data_archive/run_3/OptData
+MULTIJOB_DIR=$BASE_DIR/multijob
 ENS_STEM=runs/v8-02-01/geos5/OptData
 
 # make a directory for the raw results
 # mkdir -p $DEST_DIR/OptData
 
 # create a file to store the counts
-FILENAME=count_file_3.txt
+FILENAME=count_file_point44.txt
 touch ./$FILENAME
 
 count=0
-# for path in $MULTIJOB_DIR/ens*
-for path in $MULTIJOB_DIR/element_*
+for path in $MULTIJOB_DIR/ens*
+# for path in $MULTIJOB_DIR/element_*
 do
 
     max_val=0
-    # for sf_file in $path/$ENS_STEM/gctm.sf.*
-    for sf_file in $path/gctm.sf.*
+    for sf_file in $path/$ENS_STEM/gctm.sf.*
+    # for sf_file in $path/gctm.sf.*
     do
         opt_num=$(echo $sf_file | rev | cut -c1-2 | rev)
 
