464f9550ac132d6393088d86457c122170b7603d
Mike Stanley <mcstanle@andrew.cmu.edu>
2020-11-09 15:45:11 -0500

    added stdout and stderr saving capability to data moving script

--- monte_carlo/multijob/move_mc_full_output.sh
+++ monte_carlo/multijob/move_mc_full_output.sh
@@ -2,26 +2,40 @@
 #
 # Move the raw optimized data from the multijob monte carlo run to data archive.
 #
+# Also moves the stderr and stdout
+#
 # Author        : Mike Stanley
 # Created       : November 3, 2020
-# Last Modified : November 5, 2020
+# Last Modified : November 9, 2020
 # =============================================================================
 BASE_DIR=/glade/work/mcstanley/monte_carlo
 MULTIJOB_DIR=$BASE_DIR/multijob
 ENS_STEM=runs/v8-02-01/geos5/OptData
+FILE_STEM=runs/v8-02-01/geos5
 DEST_DIR=$BASE_DIR/data_archive/run_4
 
 # make a directory for the raw results
 mkdir -p $DEST_DIR/OptData
 
+# make a directory for the stdout and stderr files
+mkdir -p $DEST_DIR/output_files
+mkdir -p $DEST_DIR/output_files/stdout_files
+mkdir -p $DEST_DIR/output_files/stderr_files
+
 count=0
 for path in $MULTIJOB_DIR/ens*
 do
 
+    # MOVING DATA
     # make a sub directory
     mkdir -p $DEST_DIR/OptData/element_$count
 
     cp -r $path/$ENS_STEM/* $DEST_DIR/OptData/element_$count
 
+    # MOVE STDOUT AND STDERR
+    cp $path/$FILE_STEM/stdout.txt $DEST_DIR/output_files/stdout_files
+    cp $path/$FILE_STEM/stderr.txt $DEST_DIR/output_files/stderr_files
+
+    echo "Element ${count}"
     count=$((count + 1))
 done
